4a05026847d6f3464a1cf2aa1c9db0c04460dc35
joel4899 <73161763+joel4899@users.noreply.github.com>
2024-07-13 00:06:10 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,8 +31,8 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
+        title={'Plan The Perfect Party Today'}
+        subtitle={'Discover New Equipment'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
